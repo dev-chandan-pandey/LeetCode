b08862ad3fc45716a4648f000af584ac4393239f
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2024-11-13 23:17:05 +0530

    Time: 281 ms (20.93%), Space: 42 MB (66.47%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,38 +1,26 @@
 class Solution {
-    public boolean isMatch(final String s, final String p) {
-        return dfs(s, p, 0, 0, new Boolean[s.length()][p.length()]);
+  	public boolean isMatch(String s, String p) {
+        return isMatch(0,s,0,p);
     }
-
-    private boolean dfs(final String s, final String p, final int i, final int j, final Boolean[][] cache) {
-        if(i >= s.length() && j >= p.length())
-            return true;
-
-        if(j >= p.length())
-            return false;
-
-        if(i < s.length() && cache[i][j] != null)
-            return cache[i][j];
-
-        final boolean match = i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
-
-        if(j + 1 < p.length() && p.charAt(j + 1) == '*') {
-            final boolean r = dfs(s, p, i, j + 2, cache) || (match && dfs(s, p, i + 1, j, cache));
-
-            if(i < s.length())
-                cache[i][j] = r;
-
-            return r;
+    private boolean isMatch(int i, String s, int j, String p) { 
+        int sn = s.length(), pn = p.length();
+        if(j==pn) { // since * in p can match 0 of previous char, so empty string(i==sn) may match p
+            return i==sn;    
         }
-
-        if(match) {
-            cache[i][j] = dfs(s, p, i + 1, j + 1, cache);
-
-            return cache[i][j];
+        char pj = p.charAt(j);
+        if(j+1<pn && p.charAt(j+1)=='*') { //match *, needs to look at the next char to repeate current char
+            if(isMatch(i,s,j+2,p)) {
+                return true;
+            }
+            while(i<sn && (pj == '.'||pj==s.charAt(i))) {
+                if(isMatch(++i,s,j+2,p)) {
+                    return true;
+                }
+            }
+        } else if(i<sn && (s.charAt(i) == pj ||    //match char
+                   pj=='.')) {              //match dot
+            return isMatch(i+1, s, j+1, p);
         }
-
-        if(i < s.length())
-            cache[i][j] = false;
-
         return false;
     }    
 }
