489b090f306debddd80bb2b80d5e9418d8c1cf0d
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2026-05-03 21:37:26 +0530

    Time: 1 ms (23.75%), Space: 42.8 MB (58.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,6 +1,48 @@
 class Solution {
+
+    private int[] buildLPS(String pattern) {
+        int n = pattern.length();
+        int[] lps = new int[n];
+
+        int len = 0, i = 1;
+        while (i < n) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                lps[i++] = ++len;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i++] = 0;
+                }
+            }
+        }
+        return lps;
+    }
+
     public boolean rotateString(String s, String goal) {
         if (s.length() != goal.length()) return false;
-        return (s + s).contains(goal);
+
+        String text = s + s;
+        int[] lps = buildLPS(goal);
+
+        int i = 0, j = 0;
+
+        while (i < text.length()) {
+            if (text.charAt(i) == goal.charAt(j)) {
+                i++;
+                j++;
+            }
+
+            if (j == goal.length()) {
+                return true;
+            } else if (i < text.length() && text.charAt(i) != goal.charAt(j)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
     }
 }
